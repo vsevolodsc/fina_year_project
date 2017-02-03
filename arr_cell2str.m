0a472fde951568a6e9d61aa71a083e57b978865b
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-03 17:19:30 +0000

    Quicksort in progress

---
+++ arr_cell2str.m
@@ -0,0 +1,4 @@
+function [string_arr] = arr_cell2str(cell_arr)
+for i=1:length(cell_arr)
+    
+end
--- quicksort.m
+++ quicksort.m
@@ -1,9 +1,18 @@
-function [] = quicksort(arr, low, high, n)
+function [] = quicksort(hash, low, high, n)
 %Quick sort algorithm = will sort a hashmap of decoy images histograms
 %by their chi-squared distance (value) to the forged image histogram
 %return a sorted array of keys (filenames) of size n
 %credit to - http://www.programcreek.com/2012/11/quicksort-array-in-java/
-if dir == null || isempty(arr)
+values_cell = hash.values
+keys_cell = hash.keys
+for i=1:length(values_cell)
+values(i) = cell2mat(values_cell(i));
+end
+for i=1:length(keys_cell)
+values(i) = cell2mat(keys_cell(i));
+end
+
+if dir == null || isempty(hash.values)
     return;
 end
 if low >= high
@@ -11,20 +20,23 @@ if low >= high
 end
 
 middle = low + (high-low) /2;
-pivot = dir(middle);
+pivot = values(middle);
 
 i=low; j=high;
 while i <= j
-    while arr(i) < pivot
+    while values(i) < pivot
         i = i+1;
     end
-    while arr(j) < pivot
+    while values(j) < pivot
         j = j-1;
     end
     if i<=j
-        temp = arr(i);
-        arr(i) = arr(j);
-        arr(j) = temp;
+        temp = values(i);
+        temp_key = keys(i);
+        values(i) = values(j);
+        keys(i) = values(j);
+        values(j) = temp;
+        keys = temp2;
         i = i +1;
         j = j -1;
     end
