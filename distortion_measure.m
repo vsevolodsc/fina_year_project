f39fd22ef458566fd9ce7b8c8737ca23309314d0
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-06 12:10:23 +0000

    Distortion measure and pixel_remapping initialized

---
+++ distortion_measure.m
@@ -0,0 +1,5 @@
+function distortion = distortion_measure(ori, out)
+%Function measures distortion between the original(ori) image and 
+%output(out) image - forge after histogram manipulation
+
+distortion
---
+++ pixel_remapping.m
@@ -0,0 +1,4 @@
+function out = pixel_remapping(I, I2)
+%function to remap pixels accourdint to transport. function
+%Output = A forged image (I) combined with a decoy (I2)
+out
