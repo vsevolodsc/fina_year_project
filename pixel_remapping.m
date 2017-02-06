618bc9ca57b6fc7eeae4959ea610b0d776a470ab
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-06 15:37:38 +0000

    Update

--- pixel_remapping.m
+++ pixel_remapping.m
@@ -1,4 +1,7 @@
 function out = pixel_remapping(I, I2)
-%function to remap pixels according to transport. function
+% function to remap pixels according to transport. function
 %Output = A forged image (I) combined with a decoy (I2)
+forge = imread(I);
+decoy = imread(I2);
+
 out
