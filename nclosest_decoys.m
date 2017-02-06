92ccf6da9be45998cda708e407dd0ef7d1d3d892
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-06 15:26:26 +0000

    nclosest_Decoys finally done

--- nclosest_decoys.m
+++ nclosest_decoys.m
@@ -2,7 +2,8 @@ function out = nclosest_decoys(hash, n)
 %reads a hashmap - already sorted by key, where key = distance to forgery
 %returns an array on top n filenames according to values
 fnames = values(hash);
+out = cell(1,n);
 for i=1:n
-    out(i) = fnames(i)
+     out{i} = str2mat(fnames(i));
 end
-out
+
