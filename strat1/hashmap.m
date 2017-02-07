093c9ccbceca97b4b96681a4c36476242484aba0
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-07 20:07:31 +0000

    modified hashmap - now returns a list of n filenames

--- strat1/hashmap.m
+++ strat1/hashmap.m
@@ -1,6 +1,7 @@
-function [map] = hashmap_(dir, forge)
+function out = hashmap(dir, forge, n)
 % hash map generation where key -> filename
 % value -> distance to forge
+% n -> number of decoys to be returned
 n = {dir.name};
 names = {};
 for j = 3:length(n)
@@ -11,9 +12,12 @@ dir_size = length(names);
 distances = zeros(1,dir_size,'uint32');
 forge_img = imread(forge);
 str = cellstr(names);
+
 for i=1:dir_size
    decoy_img = imread(str{i});
    distance = chi_squared(imhist(forge_img), imhist(decoy_img));
    distances(i) = distance;
 end
+
 map = containers.Map(distances, str);
+out = nclosest_decoys(map, n);
