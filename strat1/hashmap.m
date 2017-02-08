ed3bd7fdd67afc8fa2e973bb5cb44753a051269b
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-08 10:06:37 +0000

    Prep for demo - WIP: select best output

--- strat1/hashmap.m
+++ strat1/hashmap.m
@@ -1,7 +1,9 @@
-function out = hashmap(dir, forge, n)
+function out = hashmap(dir, forge, N)
 % hash map generation where key -> filename
 % value -> distance to forge
 % n -> number of decoys to be returned
+%dir -> directory of decoys
+%forge -> attacked image
 n = {dir.name};
 names = {};
 for j = 3:length(n)
@@ -10,14 +12,14 @@ end
 
 dir_size = length(names);
 distances = zeros(1,dir_size,'uint32');
-forge_img = imread(forge);
+% forge_img = imread(forge);
 str = cellstr(names);
 
 for i=1:dir_size
    decoy_img = imread(str{i});
-   distance = chi_squared(imhist(forge_img), imhist(decoy_img));
+   distance = chi_squared(imhist(forge), imhist(decoy_img));
    distances(i) = distance;
 end
 
 map = containers.Map(distances, str);
-out = nclosest_decoys(map, n);
+out = nclosest_decoys(map, N);
