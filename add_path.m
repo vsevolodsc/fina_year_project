b29755f307ac166c085e96ed8710e6f3c0b79896
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-14 18:08:52 +0000

    Fixed: imwrite when converting mirflickr to gray was messing up histograms

--- add_path.m
+++ add_path.m
@@ -8,4 +8,6 @@ addpath('D:/uni-16-17/fyp/scripts');
 addpath('D:/uni-16-17/fyp/scripts/strat1');
 addpath('D:/uni-16-17/fyp/scripts/forgery');
 addpath('D:/uni-16-17/fyp/scripts/distance');
+addpath('D:/uni-16-17/fyp/scripts/strat2');
+
 
--- forgery/gamma_correction.m
+++ forgery/gamma_correction.m
@@ -1,8 +1,8 @@
-function output = gamma_correction(img_name, gamma)
+function output = gamma_correction(I, gamma)
 if gamma < 0
     gamma = 1;
 end
-I = imread(img_name);
-X = double(I);
+R = rgb2gray(I);
+X = double(R);
 output_double = 255*(X/255).^gamma;
 output = uint8(output_double);
