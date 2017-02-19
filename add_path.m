6ccf85424112b74fa42bd24e4380521c923394c9
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-19 13:40:30 +0000

    mod path

--- add_path.m
+++ add_path.m
@@ -9,6 +9,5 @@ addpath('D:/uni-16-17/fyp/scripts/strat1');
 addpath('D:/uni-16-17/fyp/scripts/forgery');
 addpath('D:/uni-16-17/fyp/scripts/distance');
 addpath('D:/uni-16-17/fyp/scripts/strat2');
-addpath('D:/uni-16-17/fyp/scripts/minlp');
 
 
