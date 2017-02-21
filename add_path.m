88d875185653251dc44c0194f49663cdcc15923b
Vsevolods Caka <vsevolods.c@gmail.com>
2017-02-21 11:22:09 +0000

    Fixed forgery

--- add_path.m
+++ add_path.m
@@ -4,6 +4,7 @@ addpath('D:/uni-16-17/fyp/images/output');
 addpath('D:/uni-16-17/fyp/images/forgery');
 addpath('D:/uni-16-17/fyp/images/decoy');
 addpath('D:/uni-16-17/fyp/images/original');
+addpath('D:/uni-16-17/fyp/images/jpeg_compress');
 addpath('D:/uni-16-17/fyp/scripts');
 addpath('D:/uni-16-17/fyp/scripts/strat1');
 addpath('D:/uni-16-17/fyp/scripts/forgery');
